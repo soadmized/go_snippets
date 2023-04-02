52540808ee8ab96044a2c52927b4a21140852dcc
soadmized <soadmized1@gmail.com>
2023-04-02 10:58:53 +0300

    is number prime exercise

---
+++ primeNumber.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func isPrime(x int) bool {
+	y := int(math.Sqrt(float64(x)))
+
+	for i := 2; i < (y + 1); i++ {
+		if (x % i) == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	x := 5
+	fmt.Printf("%d is prime: %t", x, isPrime(x))
+}
